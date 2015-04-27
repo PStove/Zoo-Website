d1b84d54ee7f13f93d3e2744d55e3514252bbbd5
gooberx21 <ian.ingram@WX-HS-6004.local>
2015-04-27 13:28:08 -0500

    .

--- Developer/code/script.js
+++ Developer/code/script.js
@@ -1,5 +1,8 @@
 var home = function() {
-    
+        document.getElementById("home").style.color="blue"
+        if (onmouseover = false) {
+            document.getElementById("home").style.color="black"
+        }
 };
 
 var about = function() {
