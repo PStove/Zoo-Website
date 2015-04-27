6c814e1beb71a8a293dd283ae7b2d23624c35aaf
gooberx21 <ian.ingram@WX-HS-6004.local>
2015-04-27 14:00:54 -0500

    .

--- Developer/code/script.js
+++ Developer/code/script.js
@@ -3,13 +3,21 @@ var home = function() {
 };
 
 var about = function() {
-
+        document.getElementById("about").style.color="blue"
 };
 
 var contact = function() {
-
+        document.getElementById("contact").style.color="blue"
 };
 
 var ian = function () {
     document.getElementById("home").style.color="black"
+};
+
+var preston = function () {
+    document.getElementById("about").style.color="black"
+};
+
+var cara = function () {
+    document.getElementById("contact").style.color="black"
 };
